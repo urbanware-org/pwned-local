09e83c4382c1ac4638698d9a533bc79a3d45fe2f
urbanware-org <github@urbanware.org>
2019-02-03 21:20:52 +0100

    Add ability to process multiple passwords

--- pwned-py3.py
+++ pwned-py3.py
@@ -9,7 +9,7 @@
 # GitLab: https://gitlab.com/urbanware-org/pwned-local
 # ============================================================================
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 import getpass
 import hashlib
@@ -30,16 +30,40 @@ def get_file_size(file_path):
     return int(file_size)
 
 
+def get_passwords():
+    """
+        Get passwords to be checked.
+    """
+    list_passwd = []
+    while True:
+        if len(list_passwd) == 1:
+            print("You can either give further passwords or just leave " +
+                  "blank and hit the")
+            print("Return key to proceed.")
+            print()
+
+        passwd = getpass.getpass("Password: ")
+        if len(passwd) == 0:
+            print("No further password given.")
+            break
+
+        hash_passwd = hashlib.sha1(passwd.encode("utf-8")).hexdigest().upper()
+        print("SHA-1 hash: " + hash_passwd)
+        list_passwd.append(hash_passwd)
+        print()
+    return list_passwd
+
+
 count = 0
 file_name = "pwned-passwords.txt"
 path_script = os.path.dirname(os.path.realpath(sys.argv[0]))
 path_list = os.path.join(path_script, file_name)
 file_size = get_file_size(path_list)
 line_len = 44   # average line length
-line_count = int(file_size / line_len)
+line_count = file_size / line_len
 pwned = False
 percent = 0
-
+retval = 0
 
 if not os.path.exists(path_list):
     print("error: Password file '%s' not found" % file_name)
@@ -68,18 +92,23 @@ if file_size < 20000000000:
 print("Enter your password below. The input will not be echoed on the " +
       "screen.")
 print()
-passwd = getpass.getpass("Password: ")
-if len(passwd) == 0:
+
+list_passwd = get_passwords()
+if len(list_passwd) == 0:
     print()
-    print("Canceled (no password given).")
+    print("Canceled (no passwords given).")
     print()
     sys.exit(0)
+elif len(list_passwd) == 1:
+    single_passwd = True
+else:
+    single_passwd = False
 
-hash_passwd = hashlib.sha1(passwd.encode("utf-8")).hexdigest().upper()
-print("SHA-1 hash: " + hash_passwd)
+print()
+print("Passwords given: " + str(len(list_passwd)))
 print()
 print("Password database size: %s bytes" % str(file_size).rjust(12))
-print("Approximate line count: %s lines" % str(line_count).rjust(12))
+print("Approximate line count: %s lines" % str(int(line_count)).rjust(12))
 print()
 print("Processing password file. Please wait as this may take a while.")
 print()
@@ -88,25 +117,37 @@ with open(path_list, "r") as f:
         if count % 1000000 == 0:
             percent = count / float(file_size / line_len) * 100
             sys.stdout.write("Progress: " + ('%6.2f' % percent) + " % (" +
-                             str(count / 1000000) + " million lines)\r")
+                             str(int(count / 1000000)) + " million lines)\r")
             sys.stdout.flush()
             time.sleep(0.002)   # reduce a bit of the CPU load
         hash_line = line.rstrip().split(":")[0]
         count += 1
-        if hash_passwd == hash_line:
-            pwned = True
-            break
+        for hash_passwd in list_passwd:
+            if hash_passwd == hash_line:
+                passwd = str(list_passwd.index(hash_passwd) + 1)
+                if single_passwd:
+                    print("Pwned! The given password has been seen before!")
+                    print("Found corresponding match in line %s." %
+                          str(count))
+                    pwned = True
+                    break
+                else:
+                    print("Pwned! Password #%s has been seen before!" %
+                          passwd)
+                    print("Found corresponding match in line %s." %
+                          str(count))
+                    print()
+                    pwned = True
 
-if pwned:
-    result = "Pwned! This password has been seen before!"
-    retval = 1
+if single_passwd:
+    if pwned:
+        retval = 1
 else:
-    result = "No pwnage found (no match in password file)."
-    retval = 0
-
-print(result)
-if pwned:
-    print("Found corresponding match in line " + str(count) + ".")
+    if pwned:
+        print("Pwned! At least one password has been seen before!")
+        retval = 1
+if not pwned:
+    print("No pwnage found (no match in password file).")
 print()
 
 sys.exit(retval)
